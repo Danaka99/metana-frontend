d66bd35fa111e290fa31f26e079df51318a9ed3e
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-09-20 23:08:17 +0530

    modify

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,6 +1,9 @@
 import { LuBox } from "react-icons/lu";
 import { FaCloud } from "react-icons/fa";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { TiThMenu } from "react-icons/ti";
+
+
 
 const WelcomeScreen = () => {
   return (
@@ -19,23 +22,17 @@ const WelcomeScreen = () => {
                 Frontend
               </a>
             </div>
-            <svg
-              className="w-5 h-5 text-gray-600 cursor-pointer hover:text-blue-600 transition duration-150"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
-              <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-            </svg>
+            <button type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="radix-:r3t:" data-state="closed" class="inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-white transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50   hover:bg-zinc-100 hover:text-zinc-900   h-7 w-7">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-4 w-4">
+               <path fill-rule="evenodd" d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a6.993 6.993 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7.047 7.047 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a6.993 6.993 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a6.993 6.993 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7.05 7.05 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54A6.992 6.992 0 0 1 7.51 3.456l.33-1.652ZM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd">
+               </path>
+              </svg>
+            </button>
           </div>
 
           {/* Segment Buttons */}
           <div className="mb-4 ant-segmented css-1jmz7pr">
-            <div className="flex space-x-1 mb-2 text-sm ant-segmented-group bg-gray-200 p-0.5 rounded">
+            <div className="flex space-x-1 mb-6 text-sm ant-segmented-group bg-gray-200 p-0.5 rounded">
               <button className="bg-gray-200 text-gray-600 hover:bg-white hover:text-gray-600 px-3 py-1 rounded">
                 Content
               </button>
@@ -51,7 +48,112 @@ const WelcomeScreen = () => {
             </div>
           </div>
 
-          <div></div>
+          <div>
+          <h3 className="flex items-center font-bold mb-2 text-sm">
+            <TiThMenu className="mr-2" />
+            Steps
+          </h3>
+
+          <p className="text-xs text-gray-500 mb-4">
+            The steps users will take to complete the form
+          </p>
+          
+          <ul className="space-y-2 text-sm">
+                  <li className="flex items-center">
+                  <button class="relative flex items-center w-full pl-1 transition duration-150  rounded-lg cursor-pointer hover:bg-zinc-100 bg-zinc-50">
+                  <div class="absolute w-2 h-8 transform -translate-y-1/2 top-1/2">
+                  </div>
+                    <button class="pl-1 pr-1.5 h-8 ">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="text-zinc-400 h-2.5 w-2.5">
+                      <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1-.75-.75v-4.5Z" clip-rule="evenodd">
+                      </path>
+                      </svg>
+                    </button>
+                    <button class="text-xs px-1.5  w-full focus:outline-none py-3">Welcome screen</button>
+                     <button class=" hidden">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-3 w-3 text-zinc-400 group-hover:text-zinc-900">
+                       <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z">
+                       </path>
+                      </svg>
+                     </button>
+                    </button>
+                  </li>
+
+                  <li className="flex items-center">
+                  <button class="relative flex items-center w-full pl-1 transition duration-150  rounded-lg cursor-pointer hover:bg-zinc-100 bg-zinc-50">
+                  <div class="absolute w-2 h-8 transform -translate-y-1/2 top-1/2">
+                  </div>
+                    <button class="pl-1 pr-1.5 h-8 ">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="text-zinc-400 h-2.5 w-2.5">
+                      <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1-.75-.75v-4.5Z" clip-rule="evenodd">
+                      </path>
+                      </svg>
+                    </button>
+                    <button class="text-xs px-1.5  w-full focus:outline-none py-3">Enter your name</button>
+                     <button class=" hidden">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-3 w-3 text-zinc-400 group-hover:text-zinc-900">
+                       <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z">
+                       </path>
+                      </svg>
+                     </button>
+                    </button>
+                  </li>
+
+                  <li className="flex items-center">
+                  <button class="relative flex items-center w-full pl-1 transition duration-150  rounded-lg cursor-pointer hover:bg-zinc-100 bg-zinc-50">
+                  <div class="absolute w-2 h-8 transform -translate-y-1/2 top-1/2">
+                  </div>
+                    <button class="pl-1 pr-1.5 h-8 ">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="text-zinc-400 h-2.5 w-2.5">
+                      <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1-.75-.75v-4.5Z" clip-rule="evenodd">
+                      </path>
+                      </svg>
+                    </button>
+                    <button class="text-xs px-1.5  w-full focus:outline-none py-3">Enter your email</button>
+                     <button class=" hidden">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-3 w-3 text-zinc-400 group-hover:text-zinc-900">
+                       <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z">
+                       </path>
+                      </svg>
+                     </button>
+                    </button>
+                  </li>
+            
+            
+            <li className="reorder-ul fill-available">
+              <button className="inline-flex  mb-6 items-center justify-center whitespace-nowrap font-medium ring-offset-white transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50   border border-zinc-200 bg-white hover:bg-zinc-100 hover:text-zinc-900 w-[90px]  rounded-lg text-xs h-7 px-2.5 gap-0.5">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-3 w-3 -ml-0.5 mr-0.5 mb-6">
+                <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z">
+                </path></svg>
+                + Add field
+              </button>
+             <div
+                class="border-t border-gray-300 my-2 w-2/ mb-8"
+                role="separator" >
+              </div>
+
+            </li>
+            <li className="flex items-center">
+              <button class="relative flex items-center w-full pl-1 transition duration-150  rounded-lg cursor-pointer hover:bg-zinc-100 bg-zinc-50">
+                  <div class="absolute w-2 h-8 transform -translate-y-1/2 top-1/2">
+                  </div>
+                    <button class="pl-1 pr-1.5 h-8 ">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" class="text-zinc-400 h-2.5 w-2.5">
+                        <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1-.75-.75v-4.5Z" clip-rule="evenodd">
+                        </path>
+                      </svg>
+                    </button>
+                    <button class="text-xs px-1.5  w-full focus:outline-none py-3">End Screen</button>
+                     <button class=" hidden">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon" class="h-3 w-3 text-zinc-400 group-hover:text-zinc-900">
+                        <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z">
+                        </path>
+                      </svg>
+                     </button>
+              </button>
+            </li>
+          </ul>
+        </div>
         </div>
       </div>
 
